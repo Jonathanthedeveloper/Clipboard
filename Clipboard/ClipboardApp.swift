123c9b0faa2015dc21107f5009ee1fcd7c77215d
Jonathan Amobi <amobijonathan2@gmail.com>
2026-01-15 22:33:05 +0100

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by Jonathan Amobi on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
